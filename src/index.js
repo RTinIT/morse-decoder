51257b874ddecb0b68ce7b4e9276b3803460e96c
rtinit <fizrukaleksikov@gmail.com>
2022-02-24 00:06:59 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,83 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let arr = []
+    getArray(expr, arr)
+    arr = deleteZeros(arr)
+    arr = getPair(arr)
+    let morse = arr.map(elem => {
+        elem = elem.map(elem => {
+            if (elem === '10') {
+                elem = '.'
+            } else if (elem === '11') {
+                elem = '-'
+            } else {
+                elem = "**"
+            }
+            return elem
+        })
+        
+        return elem
+    })
+
+    morse = morse.map(elem => elem = elem.join(''))
+
+    morse = morse.map(elem => {
+        let sign
+        for(let key in MORSE_TABLE) {
+            if (elem === key) {
+                sign = MORSE_TABLE[key]
+            }
+        }
+        return sign
+    })
+    result = morse.join('')
+
+    return result
+}
+
+
+function getArray(a, arr){
+    for (let i = 0; i < a.length; i += 10) {
+        
+        arr.push(a.substr(i,10))
+        
+    }
+    
+    return arr
+}
+
+function deleteZeros(arr) {
+    let newArr = arr.map(elem => {
+        elem = +elem
+        return elem
+    })
+    newArr = newArr.map(elem => {
+        if (isNaN(elem)) {
+            elem = '**'
+        }
+        return elem
+    })
+    newArr = newArr.map(elem => elem = String(elem))
+    return newArr
+}
+
+function getPair(arr) {
+    let res = []
+    arr.forEach((elem, i) => {
+        let newArr = []
+        for (let i = 0; i < elem.length; i+=2){
+            newArr.push(elem[i] + elem[i+1])
+        }
+        res.push(newArr)
+        return newArr
+    })
+    return res
 }
 
 module.exports = {
